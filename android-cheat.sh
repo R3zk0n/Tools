277b1ce881b57337ef52cffaac4f7f51cd41f0fa
Luke Symons <rezkon93@gmail.com>
2016-12-03 02:35:20 -0500

    Update android-cheat.sh

--- android-cheat.sh
+++ android-cheat.sh
@@ -1,5 +1,112 @@
+show_menu(){
+  clear
+  echo "BASH Script for android stuff..."
 
-echo "Start a AVD Or emulator.."
+  echo "1. Install Burp Cert"
+
+  echo "2. Install Required Lib"
+
+  echo "3. Downlad Files"
+
+  echo "4. Install APK To Mobile"
+
+  echo "5. Install .deb Files"
+
+  echo "6. Move and Unzip Files"
+
+  echo "7. Exit"
+
+}
+
+options(){
+  show_menu
+  local choice
+  read -p "Enter a choice: " choice
+  case $choice in
+    # To push the burp cerf to mobile, cant automate renaming of the cerf in mobile.
+    1) echo "Installing burp Cerf"
+    export http_proxy="http://127.0.0.1:8080"
+    wget "http://burp/cert"
+    mv cert cacert.cert
+    adb push cacert.cer /sdcard/cacert.cef
+    export http_proxy=
+    sleep 2s
+    options;
+    echo "Done..Or Not." ;;
+
+    # Installing the lib required to use drozer.
+    2) echo "Installing Libs"
+    apt-get install python-protobuf
+    echo "Done"
+    sleep 1s
+    options;;
+
+    # Downloads the files we need.
+    3) echo "Download Files"
+    if test -f "drozer-agent-2.3.4.apk"; then
+      echo "Skipping"
+    else
+    wget 'https://github.com/mwrlabs/drozer/releases/download/2.3.4/drozer-agent-2.3.4.apk';
+  fi
+
+    if test -f 'drozer_2.3.4.deb'; then
+    echo "Skipping .deb"
+  else
+    wget 'https://github.com/mwrlabs/drozer/releases/download/2.3.4/drozer_2.3.4.deb';
+  fi
+
+    if test -f "apktool_2.2.1.jar"; then
+      echo "Skipping Apktool.jar"
+    else
+    wget "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.2.1.jar";
+  fi
+
+    if test -f "download"; then
+      echo "dex2jar exists, Skipping"
+    else
+    wget "https://sourceforge.net/projects/dex2jar/files/dex2jar-2.0.zip/download";
+  fi
+
+    clear
+    echo "Done"
+    options ;;
+    # iNSTALLING APK'S TO MOIBLE DEVICE.
+    4) echo "Install APK to Mobile"
+    adb install drozer-agent-2.3.4.apk
+    adb forward tcp:31415 tcp:31415
+    drozer console connect;
+    echo "APK Pushed"
+    sleep 2s
+    options ;;
+    # INSTALLING DEB Files
+    5) echo "Install .deb Files"
+    dpkg -i drozer_2.3.4.deb
+    echo "Installed"
+    options ;;
+    # Moving and unzip files required for use..
+    6) echo "Move and unzip Files"
+    # This is moving the APKtool and Dex2Jar Files to be useable.
+    mv apktool_2.2.1.jar apktool.jar
+    cp apktool.jar /usr/local/bin
+    chmod u+x /usr/local/bin/apktool*
+    mv download dex2jar.zip
+    echo "unzipping"
+    unzip dex2jar.zip
+    echo "done"
+    options ;;
+    # exit
+    7) echo "Goodbye"
+    exit 0;;
+    # Checking..
+    *) echo -n "Not a Vaild Input.."
+    options
+  esac
+}
+
+
+
+
+echo "Checking for emulator or avd"
 adb devices | while read line
 do
     # if it's not empty, use the next line
@@ -9,51 +116,6 @@ do
         # assign device variable to the result of printing the first token
         device=`echo $line | awk '{print $1}'`
         echo "$device $@ Is up"
-        echo "Starting Setup progress.."
-        echo "Please "
-        sleep 10s
-        echo "Installing required libs.."
-        apt-get install python-protobuf
-        apt-get install -f install
-        # Setting proxy for cerf
-        export http_proxy="http://127.0.0.1:8080"
-        wget "http://burp/cert"
-        echo "Installing burp cerf.."
-        mv cert cacert.cer
-        adb push cacert.cer /sdcard/cacert.cer
-        clear;
-        export http_proxy=
-        echo "Downloading required files"
-        if test -f "drozer-agent-2.3.4.apk"; then
-        echo "File Exists..skipping drozer-apk"
-      else
-        echo "Downloading drozer-agent file."
-        wget 'https://github.com/mwrlabs/drozer/releases/download/2.3.4/drozer-agent-2.3.4.apk'
-        adb install drozer-agent-2.3.4.apk;fi
-        clear;
-        echo "Drozer Routine."
-        adb forward tcp:31415 tcp:31415
-        sleep 5s
-        drozer console connect
-        if test -f "drozer_2.3.4.deb"; then
-          echo "File Exists..Skipping drozer.deb package"
-        else
-          echo "Download drozer deb package.."
-        wget 'https://github.com/mwrlabs/drozer/releases/download/2.3.4/drozer_2.3.4.deb'
-        dpkg -i drozer_2.3.4.deb;fi
-        if test -f "apktool_2.2.1.jar"; then
-          echo "File Exists Skipping apktool.jar"
-        else
-        wget "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.2.1.jar";fi
-        mv apktool_2.2.1.jar apktool.jar
-        cp apktool.jar /usr/local/bin
-        chmod u+x /usr/local/bin/apktool*
-        if test -f "dex2jar-2.0.zip"; then
-          echo "File Exists skipping dex2jar"
-        else
-        wget "https://sourceforge.net/projects/dex2jar/files/dex2jar-2.0.zip/download";fi
-        mv download dex2jar.zip
-        echo "unzipping dex2jar"
-        unzip dex2jar.zip
       fi
-done
+    done
+  options
